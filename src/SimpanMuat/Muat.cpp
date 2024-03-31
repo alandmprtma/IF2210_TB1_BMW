43e88be13ad07354cd54aee4194da3e19f1d82a2
Albert Ghazaly <albert260306@gmail.com>
2024-03-31 21:17:21 +0700

    feat: Add Muat Tanaman

---
+++ src/SimpanMuat/Muat.cpp
@@ -0,0 +1,67 @@
+#include "Muat.hpp"
+
+
+int Muat::stringToInt(string num){
+    
+    int n = 0;
+    for (int i=0;i<num.size();i++){
+
+        n *= 10;
+        n += num[i] - '0';
+
+    }
+
+    return n;
+}
+
+std::vector<Plant> Muat::muatTanaman(string pathTanaman){
+
+        std::ifstream inputFile(pathTanaman);
+
+        if (!inputFile.is_open()){
+
+            std::cout<<"File is not found"<<std::endl;
+            
+        }else{
+            
+            std::vector<Plant> plantList = std::vector<Plant>();
+            std::string line;
+
+            while (std::getline(inputFile,line))
+            {   
+                std::istringstream iss(line);
+                // parse string dipisah spasi
+                
+                // skip id
+                std::string token;
+                std::getline(iss,token,' ');
+                
+                std::getline(iss,token,' ');
+                std::string kodeHuruf = token;
+
+                std::getline(iss,token,' ');
+                std::string nama = token;
+
+                std::getline(iss,token,' ');
+                std::string tipe = token;
+
+                std::getline(iss,token,' ');
+                int durasiPanen = stringToInt(token);
+
+                std::getline(iss,token,' ');
+                int harga =  stringToInt(token);
+
+                // simpan ke dalam list
+                plantList.push_back(Plant(
+                    kodeHuruf,
+                    nama,
+                    tipe,
+                    durasiPanen,
+                    harga
+                ));
+
+            }
+            
+        }
+
+};
---
+++ src/SimpanMuat/Muat.hpp
@@ -0,0 +1,29 @@
+#ifndef MUAT_HPP
+#define MUAT_HPP
+
+#include "Item/Plant.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
+
+
+class Muat{
+
+protected:
+
+    std::vector<Plant> plantList;
+
+public:
+
+    std::vector<Plant> muatTanaman(string pathTanaman);
+    int stringToInt(string num);
+
+};
+
+
+
+#endif
