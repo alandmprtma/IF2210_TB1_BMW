0426326d8d98fe81a9182ef656fe965167672599
MelonSeed9 <andisitorusman@gmail.com>
2024-04-01 18:23:03 +0700

    Add animal

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "./src/Item/Item.cpp"
 #include "./src/Item/Plant.cpp"
 #include "./src/Item/Produk.cpp"
+#include "./src/Item/Animal.cpp"
 // #include "Player/Petani.hpp"
 // #include "Player/Peternak.hpp"
 // #include "Player/Walikota.hpp"
@@ -9,15 +10,20 @@
 
 #include <iostream>
 using namespace std;
-int main(){
+int main (){
     Muat *muat = new Muat();
     muat->muatProduk("./Config/product.txt");
     muat->muatTanaman("./Config/plant.txt");
+    muat->muatHewan("./Config/animal.txt");
 
     // cout<<"Tanaman"<<endl;
     // for (int i=0;i<muat->getPlantList().size();i++){
     //     cout<<"Kode Huruf: "<<muat->getPlantList()[i].getKodeHuruf()<<endl;
-    //     cout<<"Nama: "<<muat->getPlantList()[i].getNama()<<endl;
+    //     cout<<"Nama: "<<muat->getPlantList()[i].getNama()<<endl;}
+    // cout<<"Hewan"<<endl;
+    // for (int i=0;i<muat->getAnimalList().size();i++){
+    //     cout<<"Kode Huruf: "<<muat->getAnimalList()[i].getKodeHuruf()<<endl;
+    //     cout<<"Nama: "<<muat->getAnimalList()[i].getNama()<<endl;
     // }
     // cout<<"Produk"<<endl;
     // for (int i=0;i<muat->getProdukList().size();i++){
