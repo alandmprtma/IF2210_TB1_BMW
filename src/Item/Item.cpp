a9293676c17aec6b18db25a2ffde3bb903cd23c7
Albert Ghazaly <albert260306@gmail.com>
2024-04-04 13:08:13 +0700

    feat: add null item

--- src/Item/Item.cpp
+++ src/Item/Item.cpp
@@ -1,14 +1,32 @@
 #include "Item.hpp"
 
+Item::Item(){
+    kodeHuruf = DEFAULT_KODE_HURUF_ITEM;    
+    nama = DEFAULT_NAMA_ITEM;
+    tipe = DEFAULT_TIPE_ITEM;
+
+}
+
 Item::Item(string kodeHuruf, string nama, string tipe):kodeHuruf(kodeHuruf),nama(nama),tipe(tipe){
 
 }
-string Item::getKodeHuruf(){
+string Item::getKodeHuruf() const{
     return kodeHuruf;
 }
-string Item::getNama(){
+string Item::getNama() const{
     return nama;
 }
-string Item::getTipe(){
+string Item::getTipe() const{
     return tipe;
+}
+bool Item::operator==(const Item& other) {
+    return this->getNama()== other.getNama() && 
+        this->getKodeHuruf()== other.getKodeHuruf() &&
+        this->getTipe()==other.getTipe();
+}
+
+Item& Item::operator=(const Item& other){
+    this->nama = other.getNama();
+    this->kodeHuruf = other.getKodeHuruf();
+    this->tipe = other.getTipe();
 }
--- src/Item/Item.hpp
+++ src/Item/Item.hpp
@@ -2,16 +2,26 @@
 #define ITEM_HPP
 #include <iostream>
 using namespace std;
+
+#define DEFAULT_KODE_HURUF_ITEM "@#$"
+#define DEFAULT_NAMA_ITEM "@#$"
+#define DEFAULT_TIPE_ITEM "@#$"
+#define NULL_ITEM Item()
+
 class Item{
 private:
     string kodeHuruf;
     string nama;
     string tipe;
 public:
+    Item();
     Item(string,string,string);
-    string getKodeHuruf();
-    string getNama();
-    string getTipe();
+    string getKodeHuruf() const;
+    string getNama() const;
+    string getTipe() const;
+    bool operator==(const Item& other);
+    Item& operator=(const Item& other);
+
 };
 
 #endif
