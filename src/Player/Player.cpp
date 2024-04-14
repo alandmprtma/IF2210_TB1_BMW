edcb4dca78a82d575060c963e2d884075e7df6e7
akmalrmn <akmalramadannn@gmail.com>
2024-04-15 01:08:20 +0700

    feat: makan

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -2,7 +2,8 @@
 
 int Player::jumlahPlayer = 0;
 
-Player::Player() {
+Player::Player()
+{
   jumlahPlayer += 1;
   this->IdPlayer = this->jumlahPlayer;
   this->uang = 50;
@@ -11,8 +12,9 @@ Player::Player() {
   this->data = PetiRahasia(8, 8); /* Untuk Sementara. Default 8 x 8 */
 }
 
-Player::Player(string username,int uang, int berat_badan, PetiRahasia data){
-  jumlahPlayer ++;
+Player::Player(string username, int uang, int berat_badan, PetiRahasia data)
+{
+  jumlahPlayer++;
   this->IdPlayer = this->jumlahPlayer;
   this->username = username;
   this->uang = uang;
@@ -21,85 +23,91 @@ Player::Player(string username,int uang, int berat_badan, PetiRahasia data){
   this->peran = "";
 }
 
-void Player::setUsername(string username) {
+void Player::setUsername(string username)
+{
   this->username = username;
 }
 
-void Player::setBeratBadan(int berat_badan) {
+void Player::setBeratBadan(int berat_badan)
+{
   this->berat_badan = berat_badan;
 }
-string Player::getUsername(){
+string Player::getUsername()
+{
   return this->username;
 }
-PetiRahasia &Player::getData(){
+PetiRahasia &Player::getData()
+{
   return this->data;
 }
 
-int Player::getUang() {
+int Player::getUang()
+{
   return this->uang;
 }
 
-void Player::setUang(int uang) {
+void Player::setUang(int uang)
+{
   this->uang = uang;
 }
 
-string Player::getPeran() {
+string Player::getPeran()
+{
   return this->peran;
 }
 
-int Player::getBeratBadan() {
+int Player::getBeratBadan()
+{
   return this->berat_badan;
 }
 
-int Player::getId(){
+int Player::getId()
+{
   return IdPlayer;
 }
-PetiRahasia Player::getPetiRahasia() {
+PetiRahasia Player::getPetiRahasia()
+{
   return this->data;
 }
 
-void Player::makan() {
-  // cout << "Pilih makanan dari penyimpanan" << endl;
+void Player::makan()
+{
+  cout << "Pilih makanan dari penyimpanan" << endl;
 
-  // petiRahasia.cetakPenyimpanan();
+  getPetiRahasia().cetakPeti("Penyimpanan");
 
-  // bool validInput = false;
+  bool validInput = false;
 
-  // while (!validInput) {
-  //   string Slot;
-  //   cout << "Slot:" << endl;
-  //   cin >> Slot;
+  while (!validInput)
+  {
+    string Slot;
+    cout << "Slot:" << endl;
+    cin >> Slot;
 
-  //   int kolom = ((int) Slot[0] - 'A') + 1;
-  //   int baris = (int) stoi(Slot.substr(1, 2));
+    int kolom = ((int)Slot[0] - 'A') + 1;
+    int baris = (int)stoi(Slot.substr(1, 2));
 
-  //   any elemen = petiRahasia.getElement(baris, kolom);
-
-  //   if (elemen.has_value()) {
-  //       if (elemen.type() == typeid(Produk)) {
-  //         Produk makanan = any_cast<Produk>(elemen);
-          
-  //         cout << "Dengan lahapnya, kamu memakan hidangan itu" << endl;
-
-  //         int beratBadanTambahan;
-  //         setBeratBadan(beratBadanTambahan);
-
-  //         cout << "Alhasil, berat badan kamu naik menjadi " << getBeratBadan() << endl;
-
-  //         validInput = true;
-  //       } else {
-  //         // cout << "Apa yang kamu lakukan??!! Kamu mencoba memakan itu?!!" << endl;
-  //         // cout << "Silahkan masukan slot yang berisi makanan." << endl;
-  //       }     
-  //   } else {
-  //       cout << "Kamu mengambil harapan kosong dari penyimpanan." << endl;
-  //       cout << "Silahkan masukan slot yang berisi makanan." << endl; 
-  //   }
-  // }
+    if (getPetiRahasia().getElement(baris - 1, kolom - 1) == nullptr)
+    {
+      std::cout << "Kamu mengambil harapan kosong dari penyimpanan." << endl
+                << "Silahkan masukan slot yang berisi makanan." << endl;
+    }
+    else if (getPetiRahasia().getElement(baris - 1, kolom - 1)->getTipe() == "Bangunan")
+    {
+      std::cout << "Apa yang kamu lakukan??!! Kamu mencoba untuk memakan itu?!!" << endl
+                << "Silahkan masukan slot yang berisi makanan." << endl;
+    }
+    else
+    {
+      break;
+    }
+    Animal *animal = dynamic_cast<Animal *>(getPetiRahasia().getElement(baris - 1, kolom - 1));
+    berat_badan += animal->getBerat();
+  }
 }
-
-void Player::printPlayer() {
-  cout << "ID Player: " << this->IdPlayer << endl; 
+void Player::printPlayer()
+{
+  cout << "ID Player: " << this->IdPlayer << endl;
   cout << "Username: " << this->username << endl;
   cout << "Uang: " << this->uang << endl;
   cout << "Berat Badan: " << getBeratBadan() << endl;
